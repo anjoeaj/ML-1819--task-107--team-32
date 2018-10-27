fd1d70b1b7e31f93a1bde41405896a1d33774a4f
cjmcmanu <cjmcm993@gmail.com>
2018-10-27 21:18:19 +0100

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,19 +2,20 @@ from sklearn.linear_model import LogisticRegression
 import numpy as np
 import pandas as pd
 from patsy import dmatrices
-
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn.model_selection import cross_val_score
+
+# NOW USING A TRAIN DATA SET AND TEST DATA SET
 
 #Read twitter data 
 dta = pd.read_csv("ML-1819--task-107--team-32_cleanedUpData.csv", ',')
 
 #Split columns to X and Y
-Y = dta.ix[:,1]
-X = dta.ix[:, 2:-1]
+Y = dta.iloc[:,1]
+X = dta.iloc[:, 2:-1]
 
-# Reformat data
+#sReformat data
 Y, X = dmatrices('gender ~ created + fav_number + tweet_count + descLen + des_hashtag_count + nameLen + tweet_length + '
                 'num_tagged + tweet_hashtags + C(has_mentioned_other_bio) + C(uses_default_link_color) + '
                 'C(tweet_has_link)', dta, return_type="dataframe")
@@ -22,11 +23,15 @@ Y, X = dmatrices('gender ~ created + fav_number + tweet_count + descLen + des_ha
 Y = np.ravel(Y)
 
 
-# instantiate a logistic regression model, and fit with X and y
+# instantiate a logistic regression model, and fit with training data for X and y
 model = LogisticRegression()
-model = model.fit(X, Y)
+model = model.fit(X_train, Y_train)
 
 # check the accuracy on the training set
 print(model.score(X, Y))
 
 print(Y.mean())
+
+# Test model on test data
+Y_pred = model.predict(X_test)
+print(model.score(X_test, Y_test))
