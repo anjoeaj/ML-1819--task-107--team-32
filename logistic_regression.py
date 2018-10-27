78a24f000daad3d4ddea2087246647a03088a4f8
rockchik <a7vi1912@gmail.com>
2018-10-27 22:18:52 +0530

    Merge branch 'master' of https://github.com/anjoeaj/ML-1819--task-107--team-32
    # Conflicts:
    #	ML-1819--task-107--team-32_cleanedUpData.csv

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,20 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-ML Twitter Data - Algorithms - Logistic Regression
-
-"""
-
+from sklearn.linear_model import LogisticRegression
 import numpy as np
 import pandas as pd
-import statsmodels.api as sm
-import matplotlib.pyplot as plt
 from patsy import dmatrices
-from sklearn.linear_model import LogisticRegression
-from sklearn.cross_validation import train_test_split
-from sklearn import metrics
-from sklearn.cross_validation import cross_val_score
 
-
-dataset = pd.read_csv('ML-1819--task-107--team-32_cleanedUpData.csv', encoding="ISO-8859-1")
-
-X=dataset[:,0]
+df = pd.read_csv("ML-1819--task-107--team-32_cleanedUpData.csv", ',')
+print(df)
+Y = df.ix[:,1]
+X = df.ix[:, 2:-1]
