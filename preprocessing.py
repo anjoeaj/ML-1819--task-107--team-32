f1e8962470c4bbb115e44ed3ae8dbf1ed06faf10
cjmcmanu <cjmcm993@gmail.com>
2018-10-27 21:12:54 +0100

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -6,7 +6,7 @@ ML Twitter Data Preprocessing
 # Importing the libraries
 import numpy as np
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 from datetime import date, datetime
 
 # Determine how many years old each account is
@@ -23,7 +23,7 @@ def CleanUp(dataset):
 
 # Functino to normalize values in data columns
 def normalizeCol(colName):
-    colVals = X[colName].values.astype(float)
+    colVals = dataset[colName].values.astype(float)
     norm = colVals/np.linalg.norm(colVals)
     return norm
 
@@ -40,52 +40,49 @@ valid = ["male", "female"]
 dataset = dataset[dataset['gender'].isin(valid)]
 dataset = dataset.reset_index(drop=True)
 
-# x will be our dataset
-X = dataset
-
 # Removing 'tweet coords' because majority are nan values
-X = X.drop(columns="tweet_coord")
+dataset = dataset.drop(columns="tweet_coord")
 
 # Removing 'tweet ID' because values are random
-X = X.drop(columns="tweet_id")
+dataset = dataset.drop(columns="tweet_id")
 
 # Removing 'tweet time' because all tweets were captured within a two minute span
-X = X.drop(columns="tweet_created")
+dataset = dataset.drop(columns="tweet_created")
 
 # Removing tweet location because it is not relevant or quanitifiable
-X = X.drop(columns="tweet_location")
+dataset = dataset.drop(columns="tweet_location")
 
 # Removing retweet_count because of insufficient data / data seems implausible
-X = X.drop(columns="retweet_count")
+dataset = dataset.drop(columns="retweet_count")
 
 # Removing user timezone because majority of the data is NaN
-X = X.drop(columns="user_timezone")
+dataset = dataset.drop(columns="user_timezone")
 
 # Removing sidebar color because default is unknown and feature is deprecated
-X = X.drop(columns="sidebar_color")
+dataset = dataset.drop(columns="sidebar_color")
 
 
 # In description columns (twitter bio), replace Nan with empty string
-X["description"] = X["description"].fillna("")
+dataset["description"] = dataset["description"].fillna("")
 
 
 # Handling categorical/text data
 # Description column (twitter bio)
 # Getting length (word count) of each description
-X["descLen"] = X["description"].str.count('\w+')
+dataset["descLen"] = dataset["description"].str.count('\w+')
 
 # Convert 'created' columns to age
 now = pd.Timestamp(datetime.now())
-X['created'] = pd.to_datetime(X['created'], format='%m/%d/%y %H:%M')
-X['created'] = X['created'].where(X['created'] < now, X['created'] - np.timedelta64(100, 'Y'))
-X['created'] = (now - X['created']).astype('<m8[Y]')
+dataset['created'] = pd.to_datetime(dataset['created'], format='%m/%d/%y %H:%M')
+dataset['created'] = dataset['created'].where(dataset['created'] < now, dataset['created'] - np.timedelta64(100, 'Y'))
+dataset['created'] = (now - dataset['created']).astype('<m8[Y]')
 
 # Find the number of hashtags used in description (twitter bio)
-X["des_hashtag_count"] = X["description"].str.count("#")
+dataset["des_hashtag_count"] = dataset["description"].str.count("#")
 
 # Has user mentioned provided a link in description(bio)
 # lower case for convenience
-lower_des = X["description"].str.lower()
+lower_des = dataset["description"].str.lower()
 bool_list = []
 # List of other social media platforms/links and abbreviations of them
 lst = ['sc:', "sc ", "snap", "insta", "ig:", "ig ", "fb:", "fb ", "facebook", "http", "https", ".com"]
@@ -96,60 +93,64 @@ for i in range(len(lower_des)):
     else:
         bool_list.append(0)
 
-X["has_mentioned_other_bio"] = bool_list
+dataset["has_mentioned_other_bio"] = bool_list
 
 # Twitter handle ("name")
 # Getting length of each name
-X["nameLen"] = X["name"].str.len()
+dataset["nameLen"] = dataset["name"].str.len()
 
 # Should be no need to replace NaN values but do it just in case
-X["nameLen"] = X["nameLen"].fillna(0)
+dataset["nameLen"] = dataset["nameLen"].fillna(0)
 
 # Identify whether default link color is used
 # 1 for default color, 0 otherwise
-X["uses_default_link_color"] = (X["link_color"] == "0084B4").astype(int)
+dataset["uses_default_link_color"] = (dataset["link_color"] == "0084B4").astype(int)
 
 # Handling a random tweet taken from the profile
 # tweet length (word count)
-X["tweet_length"] = X["text"].str.count('\w+')
+dataset["tweet_length"] = dataset["text"].str.count('\w+')
 
 # tagged any other accounts in the tweet
-X["num_tagged"] = X["text"].str.count('@')
+dataset["num_tagged"] = dataset["text"].str.count('@')
 
 # number of hashtags in the tweet
-X["tweet_hashtags"] = X["text"].str.count('#')
+dataset["tweet_hashtags"] = dataset["text"].str.count('#')
 
 # urls in tweets
-lower_tweets = X["text"].str.lower()
-X["shared_link"] = X["text"].str.contains('((http:|https:)//[^ \<]*[^ \<\.])')
+lower_tweets = dataset["text"].str.lower()
+dataset["shared_link"] = dataset["text"].str.contains('((http:|https:)//[^ \<]*[^ \<\.])')
 # double check for shorten urls
-X["shortened_urls"] = X["text"].str.contains('https?://t\.co/\S+')
+dataset["shortened_urls"] = dataset["text"].str.contains('https?://t\.co/\S+')
 # combine both
-X["tweet_has_link"] = X["shared_link"] | X["shortened_urls"]
+dataset["tweet_has_link"] = dataset["shared_link"] | dataset["shortened_urls"]
 
 # Normalizing data
-X["fav_number"] = normalizeCol("fav_number")
-X["descLen"] = normalizeCol("descLen")
-X["tweet_count"] = normalizeCol("tweet_count")
-X["nameLen"] = normalizeCol("nameLen")
-X["tweet_length"] = normalizeCol("tweet_length")
-X["created"] = normalizeCol("created")
+dataset["fav_number"] = normalizeCol("fav_number")
+dataset["descLen"] = normalizeCol("descLen")
+dataset["tweet_count"] = normalizeCol("tweet_count")
+dataset["nameLen"] = normalizeCol("nameLen")
+dataset["tweet_length"] = normalizeCol("tweet_length")
+dataset["created"] = normalizeCol("created")
 
 # Convert remaining categorical data 
 # 'True', 'False', to 1, 0
-X.tweet_has_link = X.tweet_has_link.astype(int)
+dataset.tweet_has_link = dataset.tweet_has_link.astype(int)
 
 # 'Male, 'Female' to 1, 0
-X["gender"] = np.where(X["gender"] == "male", 1, 0)
+dataset["gender"] = np.where(dataset["gender"] == "male", 1, 0)
 
 # Drop categorical data that has been processed
-X = X.drop(columns="description")
-X = X.drop(columns="link_color")
-X = X.drop(columns="name")
-X = X.drop(columns="text")
-X = X.drop(columns="shortened_urls")
-X = X.drop(columns="shared_link")
+dataset = dataset.drop(columns="description")
+dataset = dataset.drop(columns="link_color")
+dataset = dataset.drop(columns="name")
+dataset = dataset.drop(columns="text")
+dataset = dataset.drop(columns="shortened_urls")
+dataset = dataset.drop(columns="shared_link")
+
+#Split data into training and test sets
+train, test = train_test_split(dataset, test_size = 0.25, random_state = 0)
 
 #Save as csv file
-X.to_csv("ML-1819--task-107--team-32_cleanedUpData.csv", ",")
+train.to_csv("ML-1819--task-107--team-32_cleanedUpDataTrainingSet.csv", ",")
+test.to_csv("ML-1819--task-107--team-32_cleanedUpDataTestSet.csv", ",")
 
