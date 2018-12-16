9470a03485805b2ac3f5fa7b85507b838f5d0a54
anjacob <anjacob@MSC2>
2018-12-16 21:07:17 +0000

    new columns filtered

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,17 +6,33 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn.model_selection import cross_val_score
 
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+import nltk
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+import seaborn as sns # For barcharts (not used right now)
+
+#nltk.download()
+
+lemmatizer = WordNetLemmatizer()
+
 # NOW USING A TRAIN DATA SET AND TEST DATA SET
 
 #Read twitter data 
 dta = pd.read_csv("words_dataset.csv", ',')
+
+#bow_vectorizer2 = CountVectorizer(max_df=0.90, min_df=2, max_features=200, stop_words='english')
+#bow_hashtags = bow_vectorizer2.fit_transform(dta["text"])
+#for i in range(len(dta)):
+#    dta["text"][i] = bow_hashtags[i].toarray()
 
 #Split columns to X and Y
 Y = dta.iloc[:,1]
 X = dta.iloc[:, 2:-1]
 
 #sReformat data
-Y, X = dmatrices('gender ~ text', dta, return_type="dataframe")
+Y, X = dmatrices('gender ~ created + fav_number + tweet_count + descLen + nameLen', dta, return_type="dataframe")
 
 Y = np.ravel(Y)
 
@@ -33,3 +49,8 @@ print(Y.mean())
 # Test model on test data
 Y_pred = model.predict(X)
 print(model.score(X, Y))
+
+from sklearn.metrics import confusion_matrix
+tn, fp, fn, tp = confusion_matrix([0, 1, 0, 1], [1, 1, 1, 1]).ravel()
+(tn, fp, fn, tp)
+(0, 2, 1, 1)
