586640a65521c98e5874943bb262ec9998148afa
rockchik <a7vi1912@gmail.com>
2018-12-17 20:16:46 +0000

    randomized Search 68 on test and 52 on test

--- xgBoost_new.py
+++ xgBoost_new.py
@@ -1,17 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 17 15:06:36 2018
-
-@author: gargav
-"""
-
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
-from sklearn.model_selection import StratifiedFold
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import accuracy_score
 
 df = pd.read_csv("words_dataset.csv")
@@ -19,9 +12,9 @@ df = pd.read_csv("words_dataset.csv")
 X_train, X_test, y_train, y_test = train_test_split(df['text'], df['gender'],
                                                     test_size=0.25)
 
-# setting up count vectorizer
+# setting up countvectorizer
 vectorizer = CountVectorizer(stop_words='english', max_df=0.9, min_df=2,
-                             max_features=1000)
+                             max_features=3000)
 vectorized_X = vectorizer.fit_transform(X_train)
 
 print(vectorized_X.toarray())
@@ -31,7 +24,7 @@ vectorized_X_test = vectorizer.fit_transform(X_test)
 params = {
 
     # depth of tree
-    "max_depth": [4, 6],
+    "max_depth": [4, 6, 8],
 
     # min sum of weights
     # should be high enough to prevent over fitting
@@ -43,10 +36,10 @@ params = {
 
     # fraction of observations to be included in each tree
     # generally varies from 0.5-1
-    "subsample": [0.75, 0.95],
+    "subsample": [0.5, 0.75, 0.95],
 
     # fraction of column to be randomly sample in each tree
-    "colsample_bytree": [0.6, 0.95]
+    "colsample_bytree": [0.45, 0.6, 0.95]
 }
 
 """
@@ -60,19 +53,28 @@ booster = xgb.train(params, data_dmatrix)
 dtest = xgb.DMatrix(vectorized_X_test.toarray())
 """
 
-clf = xgb.XGBClassifier(base_score=0.5, colsample_bylevel=1, colsample_bytree=0.95,
-                        gamma=0.7, learning_rate=0.1, max_delta_step=0, max_depth=6,
-                        min_child_weight=10, missing=None, n_estimators=100, nthread=-1,
-                        objective='binary:logistic', reg_alpha=2e-05, reg_lambda=1,
-                        scale_pos_weight=1, seed=0, silent=True, subsample=0.75)
+clf = xgb.XGBClassifier(learning_rate=0.01, n_estimators=600,
+                        objective='binary:logistic', silent=True, nthread=1)
 
-gridsearch = GridSearchCV(clf, params)
+folds = 3
+param_comb = 5
 
-clf.fit(vectorized_X, y_train)
+skf = StratifiedKFold(n_splits=folds, shuffle=True, random_state=1001)
 
-ypred = clf.predict(vectorized_X_test)
-print(ypred)
-print(accuracy_score(y_train, ypred))
+random_search = RandomizedSearchCV(clf, params, n_iter=param_comb,
+                                   scoring='roc_auc', n_jobs=4,
+                                   cv=skf.split(vectorized_X, y_train),
+                                   verbose=3, random_state=1001)
+
+random_search.fit(vectorized_X, y_train)
+
+print(random_search.best_estimator_)
+print(random_search.best_score_)
+
+ypred = random_search.predict(vectorized_X_test)
+
+print(accuracy_score(y_test, ypred))
+
 
 
 
