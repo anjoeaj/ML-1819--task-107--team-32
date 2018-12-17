f11bf7655318add8a2c7734c42f840bcf8b892d7
anjacob <anjacob@MSC2>
2018-12-17 15:08:08 +0000

    ROC plot logic changed to use minimum variables

--- logistic_regression.py
+++ logistic_regression.py
@@ -52,8 +52,8 @@ Y = np.ravel(Y)
 # instantiate a logistic regression model, and fit with training data for X and y
 #model = LogisticRegression()
 model = OneVsRestClassifier(LogisticRegression())
-#model = model.fit(X_train, y_train)
-y_score = model.fit(X_train, y_train).decision_function(X_test)
+model = model.fit(X_train, y_train)
+#y_score = model.fit(X_train, y_train).decision_function(X_test)
 
 # check the accuracy on the training set
 print(model.score(X_train, y_train))
@@ -64,65 +64,30 @@ print(y_train.mean())
 Y_pred = model.predict(X_test)
 print(model.score(X_test, y_test))
 
+######################### PLOT ROC CURVE ###############################
 
+#Get the true positives and false positives 
+probs = model.predict_proba(X_test)
+preds = probs[:,1]
+fpr, tpr, threshold = metrics.roc_curve(y_test, preds)
+roc_auc = metrics.auc(fpr, tpr)
 
-
-
-
-# Import some data to play with
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-
-# Binarize the output
-y = label_binarize(y, classes=[0, 1, 2])
-n_classes = y.shape[1]
-
-# Add noisy features to make the problem harder
-random_state = np.random.RandomState(0)
-n_samples, n_features = X.shape
-X = np.c_[X, random_state.randn(n_samples, 200 * n_features)]
-
-# shuffle and split training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5,
-                                                    random_state=0)
-
-# Learn to predict each class against the other
-classifier = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True,
-                                 random_state=random_state))
-y_score = classifier.fit(X_train, y_train).decision_function(X_test)
-
-# Compute ROC curve and ROC area for each class
-fpr = dict()
-tpr = dict()
-roc_auc = dict()
-
-
-
-gender_arr = np.array(y_test['gender'])
-#gender_arr.ravel()
-fpr, tpr, _ = roc_curve(gender_arr, y_score)
-roc_auc = auc(fpr, tpr)
-#for i in range(n_classes):
-#    fpr[i], tpr[i], _ = roc_curve(gender_arr, y_score)
-#    roc_auc[i] = auc(fpr[i], tpr[i])
-
-# Compute micro-average ROC curve and ROC area
-fpr, tpr, _ = roc_curve(gender_arr.ravel(), y_score.ravel())
-roc_auc = auc(fpr, tpr)
-
-plt.figure()
-lw = 2
-plt.plot(fpr[2], tpr[2], color='darkorange',
-         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
-plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
+# draw the graph 
+import matplotlib.pyplot as plt
+plt.title('Receiver Operating Characteristic')
+plt.plot(fpr, tpr, 'darkorange', label = 'AUC = %0.2f' % roc_auc)
+plt.legend(loc = 'lower right')
+plt.plot([0, 1], [0, 1],'r--',color='navy')
+plt.xlim([0, 1])
+plt.ylim([0, 1])
 plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic example')
-plt.legend(loc="lower right")
+plt.xlabel('False Positive Rate')
 plt.show()
+
+######################### ROC CURVE END ###############################
+
+
+
 
 from sklearn.metrics import confusion_matrix
 tn, fp, fn, tp = confusion_matrix([0, 1, 0, 1], [1, 1, 1, 1]).ravel()
